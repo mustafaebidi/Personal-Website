038163ec77bd3358eb978bbdd5d4fb8f0c5695ed
mustafa ebid <mustafaebid123@gmail.com>
2022-06-05 15:20:20 +0200

    code optimization

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 
 
-let home = document.getElementById("home")
+let click = new Audio('../audio/Mouse Click 01.mp3');
 
-let about = document.getElementById("about")
-
-let skills = document.getElementById("skills")
-
-let work = document.getElementById("work")
-
-
-let contact = document.getElementById("contact")
-
-let bottonChangeMode = document.querySelector(".change-theme")
 
 let sections = document.querySelectorAll("section")
 
@@ -22,48 +12,7 @@ let ulNav=document.querySelector("header ul ")
 let preloaderWidget=document.querySelector(".preloader")
 let mode = false
 
-let toggleHeader=document.querySelector(".toggle-header")
 
-let click = new Audio('../audio/Mouse Click 01.mp3');
-
-
-toggleHeader.addEventListener("click",(e)=>{
-
-    if(e.target !== toggleHeader){
-        if(e.target.classList.contains("hide")){
-
-            document.querySelector(".toggle-header .show").style.transform ="scale(1)"
-            ulNav.classList.add("hide")
-
-        }
-        else{
-            document.querySelector(".toggle-header .hide").style.transform ="scale(1)"
-            ulNav.classList.remove("hide")
-
-        }
-    
-        e.target.style.transform ="scale(0)"
-
-    }
-
-
-    
-})
-
-
-
-
-allLiOfNav.forEach((element)=>{
-    element.addEventListener("click",()=>{
-        click.play();
-
-    })
-
-})
-document.querySelector(".preloader .loader-title h2:last-of-type").addEventListener('transitionend', (e) => {
-    e.target.parentNode.classList.add("is-done")
-    preloaderWidget.classList.add("is-done")
-});
 
 
 window.onload=function(){
@@ -73,8 +22,7 @@ window.onload=function(){
 
 window.onscroll = function (e) {
 
-    //console.log(this.pageYOffset)
-
+    //console.log(this.pageYOffset )
     sections.forEach((element, index) => {
 
         if (this.pageYOffset >= element.offsetTop && this.pageYOffset < (element.getBoundingClientRect().height + element.offsetTop)) {
@@ -88,17 +36,67 @@ window.onscroll = function (e) {
 
 }
 
+
+///After text fill End of preloader Page This Will Fire 
+document.querySelector(".preloader .loader-title h2:last-of-type").addEventListener('transitionend', (e) => {
+    e.target.parentNode.classList.add("is-done")
+    preloaderWidget.classList.add("is-done")
+});
+
+
+let hideHeader = document.querySelector(".toggle-header .hide");
+let showHeader = document.querySelector(".toggle-header .show");
+let hidenElemntsUl=document.querySelectorAll(".toggle-header > i ")
+
+hidenElemntsUl.forEach(element => {
+    element.addEventListener("click",(e)=>{
+
+        if(e.target.classList.contains("hide")){
+
+            showHeader.style.transform ="scale(1)"
+            ulNav.classList.add("hide")
+
+        }
+        else{
+            hideHeader.style.transform ="scale(1)"
+            ulNav.classList.remove("hide")
+
+        }
+    
+        e.target.style.transform ="scale(0)"
+
+    })
+    
+});
+
+
+
+allLiOfNav.forEach((element)=>{
+    element.addEventListener("click",()=>{
+        let sectionName=element.querySelector("a").getAttribute("section").replace("#","")
+        let secationElemet=document.getElementById(sectionName)
+        
+        window.scrollTo(0, secationElemet.offsetTop);
+
+        
+        ///Sound When i Clcik at Li on UL
+        click.play();
+
+    })
+
+})
+
+
 function removeAllActive() {
     allLinkOfNav.forEach(element => {
-
-
         element.classList.remove("active_link")
 
     });
 }
 
 
-function toggleDarkMode() {
+let bottonChangeMode = document.querySelector(".change-theme")
+function toggleMode() {
     mode = !mode
 
     document.body.classList.toggle('light')
@@ -107,7 +105,7 @@ function toggleDarkMode() {
 }
 
 bottonChangeMode.onclick = function () {
-    toggleDarkMode()
+    toggleMode()
 }
 
 

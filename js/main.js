a5173d73fc35453825de8538a126e0a95dff9370
mustafa ebid <mustafaebid123@gmail.com>
2022-06-05 09:55:58 +0200

    add preloader and add some changes in css

--- js/main.js
+++ js/main.js
@@ -1,61 +1,112 @@
 
 
-let home=document.getElementById("home")
+let home = document.getElementById("home")
 
-let about=document.getElementById("about")
+let about = document.getElementById("about")
 
-let skills=document.getElementById("skills")
+let skills = document.getElementById("skills")
 
-let work=document.getElementById("work")
+let work = document.getElementById("work")
 
 
-let contact=document.getElementById("contact")
+let contact = document.getElementById("contact")
 
-let bottonChangeMode=document.querySelector(".change-theme")
+let bottonChangeMode = document.querySelector(".change-theme")
 
-let sections=document.querySelectorAll("section")
+let sections = document.querySelectorAll("section")
 
-let allLinkOfNav=document.querySelectorAll("header ul li a")
+let allLinkOfNav = document.querySelectorAll("header ul li a")
+let allLiOfNav = document.querySelectorAll("header ul li ")
+let ulNav=document.querySelector("header ul ")
 
-let mode=false
+let preloaderWidget=document.querySelector(".preloader")
+let mode = false
 
-window.onscroll=function(e){
+let toggleHeader=document.querySelector(".toggle-header")
+
+let click = new Audio('../audio/Mouse Click 01.mp3');
+
+
+toggleHeader.addEventListener("click",(e)=>{
+
+    if(e.target !== toggleHeader){
+        if(e.target.classList.contains("hide")){
+
+            document.querySelector(".toggle-header .show").style.transform ="scale(1)"
+            ulNav.classList.add("hide")
+
+        }
+        else{
+            document.querySelector(".toggle-header .hide").style.transform ="scale(1)"
+            ulNav.classList.remove("hide")
+
+        }
+    
+        e.target.style.transform ="scale(0)"
+
+    }
+
+
+    
+})
+
+
+
+
+allLiOfNav.forEach((element)=>{
+    element.addEventListener("click",()=>{
+        click.play();
+
+    })
+
+})
+document.querySelector(".preloader .loader-title h2:last-of-type").addEventListener('transitionend', (e) => {
+    e.target.parentNode.classList.add("is-done")
+    preloaderWidget.classList.add("is-done")
+});
+
+
+window.onload=function(){
+    preloaderWidget.classList.add("active")
+
+}
+
+window.onscroll = function (e) {
 
     //console.log(this.pageYOffset)
 
-    sections.forEach((element,index) => {
+    sections.forEach((element, index) => {
 
-        if(this.pageYOffset >= element.offsetTop && this.pageYOffset < (element.getBoundingClientRect().height+element.offsetTop)  ){
+        if (this.pageYOffset >= element.offsetTop && this.pageYOffset < (element.getBoundingClientRect().height + element.offsetTop)) {
             removeAllActive()
             allLinkOfNav[index].classList.add("active_link")
-            console.log(index)
 
 
         }
-        
+
     });
 
 }
 
-function removeAllActive(){
+function removeAllActive() {
     allLinkOfNav.forEach(element => {
 
 
         element.classList.remove("active_link")
-        
+
     });
 }
 
 
-function toggleDarkMode(){
-    mode=!mode
+function toggleDarkMode() {
+    mode = !mode
 
     document.body.classList.toggle('light')
-    bottonChangeMode.setAttribute("class", `${mode ?"bx bx-moon" :"bx bx-sun" } change-theme`);
+    bottonChangeMode.setAttribute("class", `${mode ? "bx bx-moon" : "bx bx-sun"} change-theme`);
 
 }
 
-bottonChangeMode.onclick=function(){
+bottonChangeMode.onclick = function () {
     toggleDarkMode()
 }
 
